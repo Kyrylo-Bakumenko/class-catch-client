8e4bea01d846b171a4bc06dd2508f5cc9b29b1dd
Kyrylo Bakumenko <kyrylobakumenko@Kyrylos-MacBook-Pro.local>
2025-01-28 22:26:59 -0500

    production config changes attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+    env: {
+      NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL, 
+    },
+  };
 
 export default nextConfig;
